9b7494a1739b561961f7cb20f3df99aaa87083d6
Vikas Sharawat <vikas@geninf.com>
2019-12-10 20:39:40 +0530

    MulticastDelegate

---
+++ MulticastDelegate/MulticastDelegateSample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MulticastDelegate
+{
+    delegate int NumberChanger(int n);
+    class MulticastDelegateSample
+    {
+            static int num = 10;
+
+            public static int AddNum(int p)
+            {
+                num += p;
+                return num;
+            }
+            public static int MultNum(int q)
+            {
+                num *= q;
+                return num;
+            }
+            public static int getNum()
+            {
+                return num;
+            }
+            static void Main(string[] args)
+            {
+                //create delegate instances
+                NumberChanger nc;
+                NumberChanger nc1 = new NumberChanger(AddNum);
+                NumberChanger nc2 = new NumberChanger(MultNum);
+
+                nc = nc1;
+                nc += nc2;
+
+                //calling multicast
+                nc(5);
+                Console.WriteLine("Value of Num: {0}", getNum());
+                Console.ReadKey();
+            }
+        
+    }
+}
