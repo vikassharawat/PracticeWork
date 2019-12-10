89804c808f4ceb419f42677e613672abe3226bcc
Vikas Sharawat <vikas@geninf.com>
2019-12-10 17:13:46 +0530

    Rename

---
+++ DelegateSample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DelegateUse
+{
+    delegate string del(string str);
+    class sample
+    {
+        public static string DelegateSample(string a)
+        {
+            return a.Replace(',', '*');
+        }
+    }
+    class DelegateSample
+    {
+        static void Main(string[] args)
+        {
+
+            del str1 = new del(sample.DelegateSample);
+            string str = str1("Welcome,,friends,,to,,TechBeamers");
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+       
+   
+    }
+   
+}
