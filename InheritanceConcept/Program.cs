51a1051990cfae275531cf8228b0c145ecedd8e8
Vikas Sharawat <vikas@geninf.com>
2019-12-11 20:35:37 +0530

    InheritConcept

---
+++ InheritanceConcept/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InheritanceConcept
+{
+    class Program
+    {
+       
+        public static void Main()
+        {
+            classC a = new classC();
+            Console.WriteLine(a.Print());
+        }
+
+        public class classA
+        {
+            public classA()
+            {
+                Console.WriteLine("Class A");
+            }
+            public classA(string p1)
+            {
+                Console.WriteLine($"Class A with para {p1}");
+            }
+            public virtual string Print()
+            {
+                return "classA";
+            }
+        }
+
+        public class classB : classA
+        {
+            public classB():base("vikas")
+            {
+                Console.WriteLine("Class b");
+            }
+            public override string Print()
+            {
+                return "classB";
+            }
+        }
+
+        public class classC : classB
+        {
+            public classC()
+            {
+                Console.WriteLine("Class C");
+            }
+            public new string Print()
+            {
+                return "ClassC";
+            }
+        }
+    }
+}
