05e290f8538c94e9a359b468ef3a29953c89ac9d
Vikas Sharawat <vikas@geninf.com>
2019-12-12 16:00:54 +0530

    Ref,Out keywords

---
+++ RefOutKeyWords/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RefOutKeyWords
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 3;
+            int j ;
+            func1(ref i);
+            func2(out j);
+            Console.WriteLine(i + " " + j);
+            Console.ReadLine();
+        }
+
+        static void func1(ref int num)
+        {
+            num = num + num;
+        }
+        static void func2(out int num)
+        {
+            num = 2;
+            num = num * num;
+        }
+    }
+}
