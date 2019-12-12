c2eb64ece293ee3031c0e8280c77d28e5786ad0d
Vikas Sharawat <vikas@geninf.com>
2019-12-12 18:26:19 +0530

    Overriding

---
+++ Overriding/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Overriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base d = new Derived();
+            int i = 10;
+            d.Func(i);
+            Console.ReadLine();
+        }
+    }
+    public class Base
+    {
+        public virtual void Func(int x)
+        {
+            Console.WriteLine("Base.Func(int)");
+        }
+    }
+    public class Derived : Base
+    {
+        public override void Func(int x)
+        {
+            Console.WriteLine("Derived.Func(int)");
+        }
+        public void Func(object o)
+        {
+            Console.WriteLine("Derived.Func(object)");
+        }
+    }
+}
