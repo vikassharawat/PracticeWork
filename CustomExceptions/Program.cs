b69ba8b639524efe95fb4a220bfb2ed63425427e
Vikas Sharawat <vikas@geninf.com>
2019-12-12 17:23:07 +0530

    customexception

---
+++ CustomExceptions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CustomExceptions
+{
+    public class Calculation
+    {
+        int sum = 0;
+        int count = 0;
+        float average;
+        public void CalAverage()
+        {
+            if (count == 0)
+                throw (new CountIsZeroException("Zero count in DoAverage"));
+            {
+                average = sum / count;
+                Console.WriteLine("Average is :{0}",average);
+                Console.WriteLine("Program executed successfully");
+            }
+        }
+    }
+
+    public class CountIsZeroException : ApplicationException
+    {
+        public CountIsZeroException(string message) : base(message)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c = new Calculation();
+            try
+            {
+                c.CalAverage();
+            }
+            catch (CountIsZeroException e)
+            {
+                Console.WriteLine("CountIsZeroException : {0}", e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
