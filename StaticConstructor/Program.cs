c18121d5d76fd96006f3ec38dcd5a38de4b8d26f
Vikas Sharawat <vikas@geninf.com>
2019-12-12 16:48:19 +0530

    static constructor

---
+++ StaticConstructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StaticConstructor
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var test = SingletonB.Test;
+            Console.ReadLine();
+        }
+    }
+
+    class SingletonB
+    {
+        static readonly SingletonB _instance = new SingletonB();
+        public static SingletonB Test { get { return _instance; } }
+
+        private SingletonB()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+
+        static SingletonB()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+    }
+}
